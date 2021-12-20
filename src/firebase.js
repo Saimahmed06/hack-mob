b80a3877f62132de5f1c46cd67c5acdb87eb6f91
saim ahmed <saimahmed280@gmail.com>
2021-12-19 20:27:26 -0800

    final

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { getFirestore } from "firebase/firestore"
+
+import { initializeApp } from "firebase/app"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDRomuru_3Duwx50ADonOEuKbsdP0fCnEQ",
+    authDomain: "khana-sab-ky-liye.firebaseapp.com",
+    projectId: "khana-sab-ky-liye",
+    storageBucket: "khana-sab-ky-liye.appspot.com",
+    messagingSenderId: "797351327059",
+    appId: "1:797351327059:web:83a4b0fbb0cdfb7a009da6",
+    measurementId: "G-BQPMK46F2J"
+  };
+  initializeApp(firebaseConfig)
+
+export const db = getFirestore();
